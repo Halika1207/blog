f8295945bb6a2a84f51ba9aca09752c076972dee
Halyna_Velychko <130465681+Halika1207@users.noreply.github.com>
2024-01-11 17:28:22 +0100

    added helper classNames

---
+++ src/helpers/classNames.ts
@@ -0,0 +1,13 @@
+//TODO: Replace with lib classNames
+
+type Mod = Record<string, string | boolean>;
+
+export const classNames = (cls: string, mods: Mod, additionalClasses: string[]): string => {
+	return [
+		cls,
+		...additionalClasses,
+		...Object.entries(mods) //[[hovered, true], ]
+			.filter(([_, value]) => Boolean(value)) //[['hovered', true]]
+			.map(([cls, _]) => cls)
+	].join(' ');
+}
